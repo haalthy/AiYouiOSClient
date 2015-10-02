a097b1f301868a2b9b19bbfff5d2a4ced6cd1042
lilyangel <lilyangel007@gmail.com>
2015-10-01 21:55:25 -0700

    project file

---
+++ CancerSNiOSClient-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  CancerSNiOSClient-Bridging-Header.h
+//  CancerSN
+//
+//  Created by lily on 9/28/15.
+//  Copyright (c) 2015 lily. All rights reserved.
+//
+
+#ifndef CancerSN_CancerSNiOSClient_Bridging_Header_h
+#define CancerSN_CancerSNiOSClient_Bridging_Header_h
+
+
+#endif
