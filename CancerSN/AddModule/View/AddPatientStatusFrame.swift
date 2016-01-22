266f95446ed409916b86b69ad9006f88524dc869
lilyangel <lilyangel007@gmail.com>
2016-01-22 19:30:12 +0800

    add new patientstatus

---
+++ CancerSN/AddModule/View/AddPatientStatusFrame.swift
@@ -0,0 +1,20 @@
+//
+//  AddPatientStatusFrame.swift
+//  CancerSN
+//
+//  Created by hui luo on 22/1/2016.
+//  Copyright © 2016 lily. All rights reserved.
+//
+
+import Foundation
+
+//date section
+let patientStatusDateLabelLeftSpace: CGFloat = CGFloat(15)
+let patientStatusDateLabelFont: UIFont = UIFont.systemFontOfSize(13)
+let patientStatusDataLabelColor: UIColor = UIColor(red: 153 / 255, green: 153 / 255, blue: 153 / 255, alpha: 1)
+
+let patientStatusDateBtnLeftSpace: CGFloat = CGFloat(20)
+let patientStatusDateBtnFont: UIFont = UIFont.systemFontOfSize(13)
+let patientStatusDataBtnColor: UIColor = UIColor(red: 153 / 255, green: 153 / 255, blue: 153 / 255, alpha: 1)
+
+let patientStatusDateSectionHeight: CGFloat = CGFloat(44)
