0bac085a4dd683a1a9fd17ec50683c89477911eb
lilyangel <lilyangel007@gmail.com>
2016-02-24 18:27:26 +0800

    add posts abstruct

--- CancerSN/UserModule/Controller/PostsTableViewController.swift
+++ CancerSN/UserModule/Controller/PostsTableViewController.swift
@@ -9,16 +9,158 @@
 import UIKit
 
 class PostsTableViewController: UITableViewController {
+    
+    let cellPostIdentifier = "PostCell"
 
+    var username: String = ""
+    // 控件关联
+    @IBOutlet weak var headerView: UIView!
+    
+    let keychainAccess = KeychainAccess()
+    let getAccessToken = GetAccessToken()
+    let profileSet = NSUserDefaults.standardUserDefaults()
+    
+    // 自定义变量
+    
+    var postList: NSMutableArray!
+    
+    let defaultMaxID: Int = 99999999
+    var pageIndex: Int = 0
+    var countPerPage: Int  = 5
+    var maxID: Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        getAccessToken.getAccessToken()
+        if profileSet.objectForKey(accessNSUserData) == nil{
+            let storyboard = UIStoryboard(name: "Registeration", bundle: nil)
+            let rootController = storyboard.instantiateViewControllerWithIdentifier("LoginEntry") as! UINavigationController
+            self.presentViewController(rootController, animated: true, completion: nil)
+        }else{
+            initVariables()
+            initRefresh()
+            initContentView()
+        }
     }
+
+    
+    // MARK: - Init Variables
+    
+    func initVariables() {
+        
+        postList = NSMutableArray()
+        
+        
+        self.tableView.registerClass(PostTableViewCell.self, forCellReuseIdentifier: cellPostIdentifier)
+
+    }
+    
+    func getFeedListURL()->String{
+        var feedListURL: String = ""
+        let accessToken = profileSet.objectForKey(accessNSUserData)
+        if accessToken != nil{
+            feedListURL = getPostsByUsername + "?access_token=" + (accessToken as! String)
+        }
+        return feedListURL
+    }
+    
+    func getFeedListParameter(since_id: Int, max_id: Int, pageIndex: Int)-> NSDictionary{
+        let parameter = NSMutableDictionary()
+        let accessToken = profileSet.objectForKey(accessNSUserData)
+        if accessToken != nil{
+            parameter.setObject(since_id, forKey: "since_id")
+            parameter.setObject(max_id, forKey: "max_id")
+            parameter.setObject(countPerPage, forKey: "count")
+            parameter.setObject(pageIndex, forKey: "page")
+            parameter.setObject(username, forKey: "username")
+        }
+        return parameter
+    }
+    
+    // MARK: - Init Related ContentView
+    
+    func initContentView() {
+        
+        // headerView样式
+//        headerView.layer.borderWidth = 0.7
+//        headerView.layer.borderColor = UIColor.init(red: 236/255.0, green: 239/255.0, blue: 237/255.0, alpha: 1).CGColor
+        
+        // tableView 注册
+        self.tableView.registerClass(FeedCell.self, forCellReuseIdentifier: cellFeedIdentifier)
+        
+        self.tableView.mj_header.beginRefreshing()
+    }
+    
+    // MARK: - Init Refresh
+    
+    func initRefresh() {
+        
+        self.tableView.mj_header = MJRefreshNormalHeader.init(refreshingBlock: { () -> Void in
+            
+            self.getFeedListFromServer()
+            
+        })
+        
+        self.tableView.mj_footer = MJRefreshAutoNormalFooter.init(refreshingBlock: { () -> Void in
+            self.getMoreFeedListFromServer()
+        })
+        
+    }
+    
+    // MARK: - Net Request
+    
+    func getFeedListFromServer() {
+        maxID = defaultMaxID
+        self.pageIndex = 0
+        NetRequest.sharedInstance.POST(getFeedListURL(), parameters:getFeedListParameter(0, max_id: maxID, pageIndex: self.pageIndex) as! Dictionary<String, AnyObject>,
+            
+            success: { (content , message) -> Void in
+                self.pageIndex = 1
+                self.tableView.mj_header.endRefreshing()
+                self.postList.removeAllObjects()
+                let dict: NSArray = content as! NSArray
+                let homeData = PostFeedStatus.jsonToModelList(dict as Array) as! Array<PostFeedStatus>
+                if homeData.count > 0 {
+                    self.maxID = homeData[0].postID
+                    for dataItem in homeData {
+                        self.maxID = self.maxID > dataItem.postID ? self.maxID: dataItem.postID
+                    }
+                }
+                self.postList.addObjectsFromArray(homeData)
+                self.tableView.reloadData()
+                
+                
+            }) { (content, message) -> Void in
+                
+                self.tableView.mj_header.endRefreshing()
+                
+                HudProgressManager.sharedInstance.showOnlyTextHudProgress(self, title: message)
+        }
+        
+    }
+    
+    func getMoreFeedListFromServer() {
+        
+        NetRequest.sharedInstance.POST(getFeedListURL(), parameters:getFeedListParameter(0, max_id: maxID, pageIndex: self.pageIndex) as! Dictionary<String, AnyObject>,
+            
+            success: { (content , message) -> Void in
+                self.pageIndex++
+                
+                self.tableView.mj_footer.endRefreshing()
+                
+                
+                let dict: NSArray = content as! NSArray
+                let homeData = PostFeedStatus.jsonToModelList(dict as Array) as! Array<PostFeedStatus>
+                self.postList.addObjectsFromArray(homeData)
+                self.tableView.reloadData()
+                
+            }) { (content, message) -> Void in
+                
+                self.tableView.mj_footer.endRefreshing()
+        }
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -29,24 +171,27 @@ class PostsTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.postList.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellPostIdentifier, forIndexPath: indexPath) as! PostTableViewCell
+        
+        cell.post = postList[indexPath.row] as! PostFeedStatus
         return cell
     }
-    */
-
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let postCellFrame: PostCellFrame = PostCellFrame()
+        postCellFrame.post = postList[indexPath.row] as! PostFeedStatus
+        return postCellFrame.cellHeight + 30
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
---
+++ CancerSN/UserModule/View/PostCellFrame.swift
@@ -0,0 +1,49 @@
+//
+//  PostCellFrame.swift
+//  CancerSN
+//
+//  Created by hui luo on 24/2/2016.
+//  Copyright © 2016 lily. All rights reserved.
+//
+
+import UIKit
+
+class PostCellFrame: NSObject {
+    var cellHeight: CGFloat = 0
+    
+    let postContentTopSpace: CGFloat = 15
+    let postContentLeftSpace: CGFloat = 80
+    let postContentRightSpace: CGFloat = 5
+    let postContentButtomSpace: CGFloat = 15
+    let postLabelFont: UIFont = UIFont.systemFontOfSize(14)
+    let postLabelLeftSpace: CGFloat = 10
+    let postLabelTopSpace: CGFloat = 0
+
+    let postImageLength: CGFloat = 50
+
+    var post = PostFeedStatus() {
+        didSet{
+            updateUI()
+        }
+    }
+    
+    func updateUI(){
+        let postContentWidth = screenWidth - postContentLeftSpace - postContentRightSpace
+
+        //post Content
+        let postLabel = UILabel()
+        if post.hasImage > 0 {
+            cellHeight = postImageLength
+        }else{
+            let postLabelSize = post.body.sizeWithFont(postLabelFont, maxSize: CGSize(width: postContentWidth - postLabelLeftSpace * 2, height: 36))
+            postLabel.frame = CGRect(x: postLabelLeftSpace, y: postLabelTopSpace, width: postContentWidth - postLabelLeftSpace*2, height: postLabelSize.height)
+            postLabel.text = post.body
+            postLabel.numberOfLines = 2
+            postLabel.lineBreakMode = NSLineBreakMode.ByTruncatingTail
+            postLabel.sizeToFit()
+            cellHeight = postLabel.frame.height
+        }
+
+    }
+    
+}
