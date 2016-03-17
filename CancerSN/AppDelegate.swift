e4b2af381d3e95873429a0c3205c4c5365a11629
wanglei <wanglee3@163.com>
2016-03-17 17:13:36 +0800

    调用获取pushId方法

--- CancerSN/AppDelegate.swift
+++ CancerSN/AppDelegate.swift
@@ -113,6 +113,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         if registrationID != nil {
         
+            self.submitRegistrationIDToServer(registrationID)
         }
         
         NSUserDefaults.standardUserDefaults().setValue(registrationID, forKey:kDeviceToken)
